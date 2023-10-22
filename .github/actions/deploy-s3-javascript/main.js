4bb0c234e9dae0bc4963caed197c4d5f66e94264
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-10-22 01:02:07 -0300

    Add custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,6 +12,17 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
+    // 1) Get some input values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2) Upload files
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    //github.getOctokit().rest //we can use something like this (maybe not exactly like this) to send requests to GH API
+    core.notice(`Action being executed: ${github.context.action}`);
     core.notice('Hello from my custom JavaScript Action!')
 }
 
